1471484f4f8c1b220ff26548fa28474005b334f1
roll <eskarev@gmail.com>
2021-09-24 10:04:52 +0300

    Reworked the search code

--- code/search.py
+++ code/search.py
@@ -1,53 +1,61 @@
+import os
+import json
 import time
-from github import Github
-from random import randint
-from frictionless import Resource
+from dotenv import load_dotenv
+from frictionless import Resource, transform, steps
+from github import Github, RateLimitExceededException
 
 
-SLEEP = 15
-QUERY = "filename:datapackage.json size:%s..%s"
-github = Github("token", per_page=100)
+load_dotenv()
+PAUSE = 1
+RETRY = 10
+QUERY = "resources filename:datapackage.json path:/"
+github = Github(os.environ["GITHUB_TOKEN"], per_page=100)
 
 
 # Source
 
 
 def fetch_source():
-    size = 0
     source = []
+    result = github.search_code(QUERY)
+    time.sleep(PAUSE)
+    page = 0
     while True:
-
-        # Get pages
-        slice = (1 + size // 1000) * 100
-        result = github.search_code(query=QUERY % (size, size + slice - 1))
-        time.sleep(SLEEP + randint(1, SLEEP))
-        size += slice
-        if not result.totalCount:
-            break
-
-        # Iterage pages
-        page = 0
-        while True:
+        try:
             items = result.get_page(page)
-            time.sleep(SLEEP + randint(1, SLEEP))
-            page += 1
-            if not items:
-                break
-            for item in items:
-                file = {}
-                file["user"] = item.repository.owner.login
-                file["repo"] = item.repository.name
-                file["path"] = item.path
-                file["url"] = item.url
-                source.append(file)
-            print(f"Found items: {len(source)}")
-        break
-
+        except RateLimitExceededException:
+            time.sleep(RETRY)
+            continue
+        time.sleep(PAUSE)
+        page += 1
+        if not items:
+            break
+        for item in items:
+            repo = item.repository
+            file = {}
+            file["user"] = repo.owner.login
+            file["repo"] = repo.name
+            file["branch"] = repo.default_branch
+            file["path"] = item.path
+            file["stars"] = repo.stargazers_count
+            file["download_url"] = item.download_url
+            try:
+                file["content"] = json.dumps(json.loads(item.decoded_content))
+            except json.JSONDecodeError:
+                continue
+            source.append(file)
+        print(f"Found items: {len(source)}")
     return source
 
 
 # General
 
 
-resource = Resource(fetch_source())
-resource.write("data/search.csv")
+transform(
+    Resource(fetch_source()),
+    steps=[
+        steps.row_sort(field_names=["stars"], reverse=True),
+        steps.table_write(path="data/packages.csv"),
+    ],
+)
